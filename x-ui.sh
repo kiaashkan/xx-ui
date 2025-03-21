2f7eadd188bc7e15d3072ae69bb274715f61e072
Kia <161665236+kiaashkan@users.noreply.github.com>
2025-03-21 17:09:36 +0100

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -353,7 +353,7 @@ enable_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/MHSanaei/3x-ui/raw/main/x-ui.sh
+    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/kiaashkan/xx-ui/raw/main/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         LOGE "Failed to download script, Please check whether the machine can connect Github"
